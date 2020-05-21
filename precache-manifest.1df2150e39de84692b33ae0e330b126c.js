28e5fead09733f2a6e17fd41fa729f17878e675d
Moonlarya <gum225@gmail.com>
2020-05-21 17:40:21 +0300

    Updates

---
+++ precache-manifest.1df2150e39de84692b33ae0e330b126c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6956cd88c4d44bc0de2c76ff1c7793e2",
+    "url": "/simple-todolist/index.html"
+  },
+  {
+    "revision": "22ece797e0716433376f",
+    "url": "/simple-todolist/static/css/main.4a286cee.chunk.css"
+  },
+  {
+    "revision": "db96a8e868d87e0ab124",
+    "url": "/simple-todolist/static/js/2.cd5ce978.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todolist/static/js/2.cd5ce978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ece797e0716433376f",
+    "url": "/simple-todolist/static/js/main.eded0ef4.chunk.js"
+  },
+  {
+    "revision": "a049444dfcb74f4ccd85",
+    "url": "/simple-todolist/static/js/runtime-main.e0179f5b.js"
+  }
+]);
